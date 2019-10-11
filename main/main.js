0ad99f07f3196b23a9e2a90595be0e1f97af9b2e
LYROL LORENZO (GEDI-ISD-OOCLL/MNL) <LORENLY@OOCL.COM>
2019-10-11 13:40:19 +0800

    modified main class

--- main/main.js
+++ main/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-//TODO: Implement the exercise requirements in this file and remove this comment
+function printReceipt(tags){
+	var test = tags.map(barcode => ({barcode}));
+	console.log(test);
+	// return ;
+}
+
+
+function decodeTags(tags){
+	decodeBarcodes(tags);
+}
+
+function decodeBarcodes(){
+	var decodedBarcodes;
+	return decodedBarcodes;
+}
+
+//https://playcode.io/
+   /* const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ];
+
+    	var items = tags.map(barcode => ({barcode}).barcode.split('-'));
+	console.log(items);*/
